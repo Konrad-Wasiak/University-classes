3d3d290c25dca7701e0e8b15bc4ff8c4a0e2cc14
Konrad Wasiak <83837@student.ue.poznan.pl>
2024-04-29 10:23:26 +0200

    Formatting

--- Advanced_Python_class/24_04/Z1.py
+++ Advanced_Python_class/24_04/Z1.py
@@ -1,3 +1,5 @@
+# Task: download and plot a chart of candles.
+
 import requests
 import json
 import pandas as pd
@@ -14,25 +16,30 @@ def process_candles_data(candles_data):
     for candle in candles_data:
         new_candle = {}
         new_candle_elements = [
-            ('time', candle[0]), ('open', float(candle[1])), ('close', float(candle[2])), ('high', float(candle[3])), ('low', float(candle[4]))
+            ('time', candle[0]), ('open', float(candle[1])), ('close', float(candle[2])),
+            ('high', float(candle[3])), ('low', float(candle[4]))
         ]
         new_candle.update(new_candle_elements)
         formatted_candles_data.append(new_candle)
     return formatted_candles_data
 
 
-formatted_candles_data = process_candles_data(candles_data)
+def plot_candles(formatted_candles_data):
+    df = pd.json_normalize(formatted_candles_data) # przetwarzamy dane w formacie json
+    df.time = pd.to_datetime(df.time, unit='s') # ponieważ oczekiwany format daty w kolumnie "time" jest inny niż oczekiwany przez bibliotekę to musimy go skonwertować
+    df = df.set_index("time") # ustawiamy index naszych danych na kolumnę "time"
 
 
-df = pd.json_normalize(formatted_candles_data) # przetwarzamy dane w formacie json
-df.time = pd.to_datetime(df.time, unit='s') # ponieważ oczekiwany format daty w kolumnie "time" jest inny niż oczekiwany przez bibliotekę to musimy go skonwertować
-df = df.set_index("time") # ustawiamy index naszych danych na kolumnę "time"
-print(df.head())
+    mpl.plot(
+        df, # przekazujemy obiekt z danymi
+        type="candle", # określamy typ wykresu.
+        title="Candle chart", # nadajemy tytuł naszego wykresu.
+        style="binance", # wersja kolorystyczna wykresu.
+        mav=(3, 6, 9) # atrybut który włączy automatyczne obliczanie oraz rysowanie średni kroczących.
+    )
 
-mpl.plot(
-    df, # przekazujemy obiekt z danymi
-    type="candle", # określamy typ wykresu.
-    title="Candle chart", # nadajemy tytuł naszego wykresu.
-    style="binance", # wersja kolorystyczna wykresu.
-    mav=(3, 6, 9) # atrybut który włączy automatyczne obliczanie oraz rysowanie średni kroczących.
+plot_candles(
+    process_candles_data(
+        candles_data
+    )
 )
